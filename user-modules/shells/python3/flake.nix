e66f62dbd5c53628c9d0f0ea7e2e7b6822ebe60e
lachlan-deckRMIT <112921022+lachlan-deckRMIT@users.noreply.github.com>
2026-02-11 14:03:05 +1100

    added a python 3 shell and a dir for more shell flakes

---
+++ user-modules/shells/python3/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Standalone Python dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "aarch64-darwin"; # or aarch64-darwin, x86_64-darwin, etc
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        pkgs.python3
+      ];
+
+      shellHook = ''
+        echo "🐍 Python dev shell ready"
+        python --version
+      '';
+    };
+  };
+}
