5bf3a34003da7e1f8b0d08a2cc9d9a38a4d3bbf3
lachlan-deckRMIT <112921022+lachlan-deckRMIT@users.noreply.github.com>
2026-05-18 07:51:29 +1000

    added algos shell

---
+++ user-modules/shells/algo/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Standalone Python dev shell for algo/analysis assignment";
+
+  # nix develop ~/lachNix/user-modules/shells/python3
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "aarch64-darwin"; # or aarch64-darwin, x86_64-darwin, etc
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        pkgs.python313
+        pkgs.python313Packages.matplotlib
+      ];
+
+      shellHook = ''
+        echo "🐍 Algos Python dev shell ready"
+        python --version
+      '';
+    };
+  };
+}
