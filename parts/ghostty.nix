75ca056ce1aa638b39e4e8cb320cafa1a91b795c
lachlan-deckRMIT <112921022+lachlan-deckRMIT@users.noreply.github.com>
2026-06-19 15:45:39 +1000

    updated colors

--- parts/ghostty.nix
+++ parts/ghostty.nix
@@ -37,23 +37,24 @@
           cursor-color = "928374";
           selection-background = "504945";
           selection-foreground = "ebdbb2";
+
           palette = [
-            "0=#282828" # black
-            "1=#cc241d" # red
-            "2=#98971a" # green
-            "3=#d79921" # yellow
-            "4=#458588" # blue
-            "5=#b16286" # magenta
-            "6=#689d6a" # cyan
-            "7=#a89984" # white
-            "8=#928374" # bright black
-            "9=#fb4934" # bright red
-            "10=#b8bb26" # bright green
-            "11=#fabd2f" # bright yellow
-            "12=#83a598" # bright blue
-            "13=#d3869b" # bright magenta
-            "14=#8ec07c" # bright cyan
-            "15=#ebdbb2" # bright white
+            "0=#282828" # 235 bg
+            "1=#cc241d" # 124 red
+            "2=#98971a" # 106 green
+            "3=#d79921" # 172 yellow
+            "4=#458588" # 66  blue
+            "5=#b16286" # 132 purple
+            "6=#689d6a" # 72  aqua
+            "7=#a89984" # 246 gray
+            "8=#928374" # 245 gray
+            "9=#fb4934" # 167 red
+            "10=#b8bb26" # 142 green
+            "11=#fabd2f" # 214 yellow
+            "12=#83a598" # 109 blue
+            "13=#d3869b" # 175 purple
+            "14=#8ec07c" # 108 aqua
+            "15=#ebdbb2" # 223 fg1
           ];
         };
       };
